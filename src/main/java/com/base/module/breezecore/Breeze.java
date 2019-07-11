a0f75a1e2b21513b2ae77c2da9d439b1a2997b0f
wangyannan <ynwang@grandstream.cn>
2019-07-11 15:07:08 +0800

    init breeze core

---
+++ src/main/java/com/base/module/breezecore/Breeze.java
@@ -0,0 +1,10 @@
+package com.base.module.breezecore;
+
+import android.content.Context;
+
+public class Breeze {
+    public static boolean init(Context context) {
+        if (context != null) return true;
+        return false;
+    }
+}
